5454201c6410bcd2377c86af3fecc073e2af8349
Laurent Meunier <laurmeun.business@gmail.com>
2022-07-14 11:05:15 +0200

    chore: up version

--- node/build.sh
+++ node/build.sh
@@ -1,2 +1,7 @@
 #!bin/bash
-docker build -t starknet-node:0.0 --build-arg PATHFINDER_VERSION="v0.2.2-alpha" .
+docker build -t starknet-node:1.0 --build-arg PATHFINDER_VERSION="v0.2.2-alpha" .
+if [ ! -z "$1" ]
+then
+   docker tag starknet-node:latest alphaticks/starknet-node:$1
+   docker push alphaticks/starknet-node:$1
+fi
